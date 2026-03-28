997cf22f954a4719c145ba687d96beabcfdbce24
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2026-03-28 11:32:21 -0400

    chore: bump effortless to v0.12.5

--- Casks/effortless.rb
+++ Casks/effortless.rb
@@ -1,6 +1,6 @@
 cask "effortless" do
-  version "0.12.4"
-  sha256 "5d1840a870b43ccdf6f55700225df3d6015a83d016588171e56482a47efe467b"
+  version "0.12.5"
+  sha256 "3fa4b9fea3d62f701bc86f02b1d5fe844bf41833ea8bb4c488c5666cbec1cddd"
 
   url "https://github.com/iulspop/effortless-attention/releases/download/v#{version}/Effortless.zip"
   name "Effortless"
